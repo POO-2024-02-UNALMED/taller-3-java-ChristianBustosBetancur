56e9154d618fcbecaec40640ace20cf49639869e
Christian Bustos Betancur <111025495+ChristianBustosBetancur@users.noreply.github.com>
2024-11-13 20:50:28 +0000

    Prueba2

--- src/main/java/TV.java
+++ src/main/java/TV.java
@@ -79,27 +79,27 @@ public class TV {
 	}
 	
 	public void canalUp() {
-		if(canal <= 120 
-				&& estado == true) {
+		if(estado == true
+				&& canal <= 120) {
 			canal++;
 		}			
 	}
 
 	public void canalDown() {
-		if(canal> 1 
-				&& estado == true) {
+		if(estado == true
+				&& canal> 1 ) {
 			canal--;
 		}
 	}
 	public void volumenUp() {
-		if (volumen <=7 
-				&& estado == true) {
+		if (estado == true  
+				&& volumen <=7) {
 			volumen++;
 		}
 	}
 	public void volumenDown() {
-		if (volumen>0
-				&& estado == true) {
+		if (estado == true
+				&& volumen>0) {
 			volumen--;
 		}
 	}
