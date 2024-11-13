3b598a8e9411599496969ef11e028a39f6892ca6
Christian Bustos Betancur <111025495+ChristianBustosBetancur@users.noreply.github.com>
2024-11-13 22:15:57 +0000

    Prueba5.9

--- src/main/java/Control.java
+++ src/main/java/Control.java
@@ -25,37 +25,37 @@ public class Control {
 		}
 	}
 	public void canalUp() {
-		if(tv.getEstado() == true && canal > 1 && canal <= 120) {
+		if(tv.getEstado()== true) {
 			tv.canalUp();
 		}
 		
 	}
 	public void canalDown() {
-		if(tv.getEstado()== true && canal > 1 && canal <= 120) {
+		if(tv.getEstado()== true) {
 			tv.canalDown();
 		}
 	}
 	
 	public void volumenUp() {
-		if(tv.getEstado()== true & volumen > 0 && volumen <= 7) {
+		if(tv.getEstado()== true) {
 			tv.volumenUp();
 		}
 	}
 	public void volumenDown() {
-		if(tv.getEstado()== true & volumen > 0 && volumen <= 7) {
+		if(tv.getEstado()== true) {
 			tv.volumenDown();
 		}
 	}
 	
 	public void setCanal(int canal) {
 		if(tv.getEstado()==true
-				&& canal > 1 && canal <= 120) {
+				) {
 			tv.setCanal(canal);
 		}
 	}
 	public void setVolumen(int volumen) {
 		if(tv.getEstado()==true 
-				&& volumen > 0 && volumen <= 7) {
+				) {
 			tv.setVolumen(volumen);
 		}	
 	}
